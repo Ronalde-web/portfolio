9e429fd6cb7de35d430724cdf0ad2040bfa2a7bb
Ronalde Gonçalves <ronalderota@gmail.com>
2023-12-13 15:02:11 -0300

    HTML e CSS prontos

---
+++ assets/js/acordeon.js
@@ -0,0 +1,8 @@
+const acordeonTriggers = document.querySelectorAll('.acordeon .abilidades')
+
+acordeonTriggers.forEach((abilidades) => {
+    abilidades.addEventListener('click', (e) => {
+        debugger
+        console.log('show')
+    })
+})
